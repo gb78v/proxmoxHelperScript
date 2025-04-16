c59f03356178c1c841f230ca419c10e1df2e53a3
gb78v <gregory.barriere@ac-versailles.fr>
2025-04-16 22:19:01 +0200

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/gb78v/proxmoxHelperScript/refs/heads/main/jellyfin2)
+source <(curl -s https://raw.githubusercontent.com/gb78v/proxmoxHelperScript/refs/heads/main/jellyfin2build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
